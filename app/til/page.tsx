8205207c6b7ae276731787a79f4734774a92d1a6
Victor Dosca <remmark999@gmail.com>
2025-04-05 14:42:31 +0500

    init

---
+++ app/til/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import First from "@/notes/first.mdx";
+
+export default function TILPage() {
+  return (
+    <div className="prose">
+    <First />
+    </div>
+  )
+}
+
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  // Optionally provide remark and rehype plugins
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
