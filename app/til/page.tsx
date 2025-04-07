377db95c96d8a22fde319e0812c50096af72c5b4
Victor Dosca <remmark999@gmail.com>
2025-04-07 11:43:56 +0500

    Add til note, display links in column

--- app/til/page.tsx
+++ app/til/page.tsx
@@ -5,12 +5,16 @@ const links = [
   {
     title: "First note",
     url: "/first"
+  },
+  {
+    title: "Daemonizing Node apps",
+    url: "/daemonizing-node-apps"
   }
 ]
 
 export default function TILPage() {
   return (
-    <div>{
+    <div className='flex flex-col'>{
       links.map((link) => <Link key={link.url} href={`til/${link.url}`}>{link.title}</Link>)
     }</div>
   )
